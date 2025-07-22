90508f3192efa12cf30164363411a4ad0bf50b0f
nekotoro-mod <kitsuneko.info@gmail.com>
2025-07-22 19:48:53 +0900

    Update start.sh

--- minecraft-server/start.sh
+++ minecraft-server/start.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Paperの最新バージョンをダウンロード（1.21.1）
+curl -o server.jar https://api.papermc.io/v2/projects/paper/versions/1.21.1/builds/173/downloads/paper-1.21.1-173.jar
+
+# 規約同意
+echo "eula=true" > eula.txt
+
+# サーバー起動
+java -Xmx1G -Xms1G -jar server.jar nogui
